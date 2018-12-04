aa67a74abfabcbc079c414da1ec9777cfcc0c93a
Scott Allen Cambo <scottcambo2019@u.northwestern.edu>
2018-12-04 12:41:07 -0600

    cleaning up server.py

--- server.py
+++ server.py
@@ -9,28 +9,16 @@ app.debug = True
 
 @app.route('/')
 def send_index():
-    return send_from_directory('static/static_html', 'index.html')
-
-@app.route('/map')
-def send_static_html():
-	return send_from_directory('static/static_html', 'map.html')
-
-@app.route('/map2')
-def send_static_html2():
-	return send_from_directory('static/static_html', 'map2.html')
-
-@app.route('/js/draw-map.js')
-def send_js():
-	return send_from_directory('static/js/', 'draw-map.js')
-'''
-@app.route('/css/example.css')
-def send_css(path):
-    return send_from_directory('static/css', path)
-'''
+    ''' send_index() returns the html file to display the map.
+        This file includes all the javascript necessary to do the 
+        Plotly rendering. '''
+    return send_from_directory('static/static_html', 'map3.html')
 
 @app.route('/csv/city-ratio-file.csv')
-def send_csv(path):
-    return send_from_directory('static/csv', path)
+def send_csv():
+    '''send_csv() returns the csv file containing the most
+    recent data from Google Trends.'''
+    return send_from_directory('static/csv/', 'city-ratio-file.csv')
 
 if __name__ == "__main__":
     host_loc = "127.0.0.1"
